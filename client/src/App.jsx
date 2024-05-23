4398605f1c20e7738bb0bffc7c752650bb360c88
Afrem05 <zweibis5plus0@gmail.com>
2024-05-23 06:35:29 +0200

    Project Setup 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react'
+import {
+  Navbar,
+  Welcome,
+  Services,
+  Transactions,
+  Footer,
+  Loader,
+} from "./components";
 
 const App = () => {
-
   return (
-    <div>
-      <p>App</p>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
